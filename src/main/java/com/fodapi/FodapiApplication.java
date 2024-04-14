587dc1b84503b8b8a337b0a3f86b1ab352a8101c
tolku <91892836+tolku@users.noreply.github.com>
2024-04-14 14:48:01 +0200

    +base of a project

---
+++ src/main/java/com/fodapi/FodapiApplication.java
@@ -0,0 +1,13 @@
+package com.fodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FodapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FodapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fodapi/FodapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FodapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
